a40f9ff23e24a090f5558017a6e8dda467ca22ee
lichen <lichen0426@stu.xidian.edu.cn>
2022-03-23 11:31:51 +0800

    🔧 Create Typescript shell

---
+++ createTypescriptEnv.sh
@@ -0,0 +1,10 @@
+# /usr/bin/zsh or /usr/bin/bash
+# Both are OK.
+# @author: CharryLee
+# @date: 2022-03-23
+# @description: Create a Typescript environment
+#               in your current directory or your
+#               declared directory.
+# @bug:
+
+version="0.1.0"
