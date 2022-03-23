6408740954d7c1d3bb3fec590f348363bf8c5852
lichen <lichen0426@stu.xidian.edu.cn>
2022-03-24 00:14:02 +0800

    🔧 Add tip text color

--- createPyVenv.sh
+++ createPyVenv.sh
@@ -10,6 +10,81 @@
 #               3. macOS environment conflict: can't assume zsh as default;
 #               4. tips grammar wrong;
 
+###################### #Color Text Setting ################################
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+###########################################################################
+
 version="1.0.0"
 options="$1"
 directory="$2"
@@ -70,8 +145,11 @@ trusted-host = mirrors.aliyun.com" >> ./$venvPATH/pip.conf
     echo "🍺 virtual environment starting..."
     echo "🎉 virtual envirmonment started..."
     echo "🔔 You can start virtual environment by 
-             source $directory/$venvPATH/bin/activate"
-    echo "🔔 You can use deactivate to stop the virtual environment."
+
+             $Green source $directory/$venvPATH/bin/activate $Color_Off
+
+"
+    echo "🔔 You can use $Red deactivate $Color_Off to stop the virtual environment."
 }
 
 if [ $options = "-v" -o $options = "--version" ]
@@ -88,7 +166,7 @@ then
 
     if [ -z $directory ]
     then
-        echo "❌ Error: missing declared directory."
+        echo "❌ $Red Error $Color_Off: missing declared directory."
     else
         dirInfo=`cd $directory`
         if [ -z $dirInfo ]
@@ -103,7 +181,7 @@ then
                     echo "
                 # added by createPyVenv.sh
                 alias pev=${shellPATH}/createPyVenv.sh" >> ~/.bashrc
-                    echo "🤗 You can use it by command pev after restart your terminal."
+                    echo "🤗 You can use it by command $Yellow pev $Color_Off after restart your terminal."
                 fi
             else
                 zshrc=`cat ~/.zshrc`
@@ -112,7 +190,7 @@ then
                     echo "
                 # added by createPyVenv.sh
                 alias pev=${shellPATH}/createPyVenv.sh" >> ~/.zshrc
-                    echo "🤗 You can use it by command pev after restart your terminal."
+                    echo "🤗 You can use it by command $Yellow pev $Color_Off after restart your terminal."
                 fi
             fi
         else
